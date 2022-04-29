d455a19aa99a70cca2beba588c3488d5e73d2d5f
KhantPhone <khantphone333@email.com>
2022-04-30 02:26:16 +0630

    new arrivals section

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,32 @@
         toTop.classList.remove("active");
      }
     });
+
+  // $(document).ready(function(){
+
+  //   $search = $('.fa-search');
+  //   $searchInput = $('#search-input');
+  //   $searchClose = $('.fa-close');
+
+   
+  //   $search.click(function (){
+    
+  //     $searchInput.show();
+
+  //   })
+
+  //   $searchClose.click(function (){
+  //     $searchInput.hide();
+  //   })
+
+  // })
+
+  /*mixitup js*/
+  var mixer = mixitup('.works', {
+        selectors: {
+            target: '.col-md-4'
+        },
+        animation: {
+            duration: 450
+        }
+    });
